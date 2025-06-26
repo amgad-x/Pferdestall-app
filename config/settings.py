5dd76d98df1757c6cd08431fc7260eef6363e031
s0584707 <Amgad.al-qah@Student.HTW-Berlin.de>
2025-06-26 20:51:00 +0200

    Add Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pferdestall-app.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
